887d0e08c68758f43f4e2d118632ba73fc515751
Nick Kleiner <nklei25@gmail.com>
2025-11-24 19:23:48 -0500

    Update DeviceIdHelper.cs
    Added extra logging to the DeviceIdHelper to try and find the issue of weird device crashes

--- Ares.Core/Device/Helpers/DeviceIdHelper.cs
+++ Ares.Core/Device/Helpers/DeviceIdHelper.cs
@@ -1,10 +1,22 @@
 namespace Ares.Core.Device.Helpers;
+using Microsoft.Extensions.Logging;
 
-public class DeviceIdHelper(IDeviceCommandInterpreterRepo deviceCommandInterpreterRepo)
+public class DeviceIdHelper(IDeviceCommandInterpreterRepo deviceCommandInterpreterRepo, ILogger<DeviceIdHelper> logger)
 {
   public string DeviceIdToName(string id)
   {
-    var device = deviceCommandInterpreterRepo.First(dci => dci.Device.UniqueId == id);
+    var device = deviceCommandInterpreterRepo.FirstOrDefault(dci => dci.Device.UniqueId == id);
+    if(device == null)
+    {
+      logger.LogDebug($"Device was not found in the interpreter repo with id of {id}");
+      logger.LogDebug($"The following devices are found in the repo");
+      foreach(var interpreter in deviceCommandInterpreterRepo)
+      {
+        logger.LogDebug($"{interpreter.Device?.UniqueId ?? \"Unknown id\"} - {interpreter.Device?.Name ?? \"Unknown name\"}");
+      }
+
+      return "";
+    }
     return device.Device.Name;
   }
 }
