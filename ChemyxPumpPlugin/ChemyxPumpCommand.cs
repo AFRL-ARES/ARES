84133dd84d30675bc32a5f8f975c70004eee7bca
Arnas Babeckis <xxl606@gmail.com>
2025-12-09 01:34:49 -0500

    Had the "optimizer" create a sim chemyx pump

--- ChemyxPumpPlugin/ChemyxPumpCommand.cs
+++ ChemyxPumpPlugin/ChemyxPumpCommand.cs
@@ -5,5 +5,6 @@ public enum ChemyxPumpCommand
   StartPump,
   StopPump,
   PausePump,
-
+  PumpStatus,
+  
 }
--- ChemyxPumpPlugin/Simulation/SimChemyxPumpConnection.cs
+++ ChemyxPumpPlugin/Simulation/SimChemyxPumpConnection.cs
@@ -1,20 +1,21 @@
 ﻿using Ares.Device.Serial;
 using Ares.Device.Serial.Simulation;
 using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Text;
 
 namespace ChemyxPumpPlugin.Simulation;
 
 internal class SimChemyxPumpConnection : AresSerialSimConnection, IChemyxPumpConnection
 {
+    private readonly SimChemyxPump _pump;
+
     public SimChemyxPumpConnection(string portName) : base(new SerialPortConnectionInfo(0, Parity.None, 0, StopBits.None), portName, new SerialConnectionOptions { SendBuffer = TimeSpan.FromMilliseconds(150) })
     {
+        _pump = new SimChemyxPump(AddDataReceived);
     }
 
     public override void SendInternally(byte[] bytes)
     {
-        throw new NotImplementedException();
+        _pump.SendCommand(bytes);
     }
 }
