e25b06cb98bcf72858ce18e8b4556af19af3e569
Arnas Babeckis <xxl606@gmail.com>
2026-02-26 22:24:39 -0500

    Merged most of Program from Service

---
+++ UI/Infrastructure/Grpc/ServiceMapper.cs
@@ -0,0 +1,16 @@
+﻿using Ares.Services;
+using Microsoft.AspNetCore.Authentication;
+using AresScriptingService=Ares.Core.Grpc.Services.AresScriptingService;
+using DeviceStateExportService=Ares.Core.Grpc.Services.DeviceStateExportService;
+
+namespace UI.Infrastructure.Grpc;
+
+public static class ServiceMapper
+{
+  public static void MapAresServices(this IEndpointRouteBuilder routeBuilder)
+  {
+    routeBuilder.MapGrpcService<AuthenticationService>();
+    routeBuilder.MapGrpcService<DeviceStateExportService>();
+    routeBuilder.MapGrpcService<AresScriptingService>();
+  }
+}
